1a7d753a88635d35fc734c81beceb6e6c5322af2
nataliasanchezsanchez <47853585+nataliasanchezsanchez@users.noreply.github.com>
2023-08-25 00:41:55 +0530

    Update nlp-agent.service.ts

--- src/app/services/nlp-agent.service.ts
+++ src/app/services/nlp-agent.service.ts
@@ -1,9 +1,5 @@
-import { publishFacade } from '@angular/compiler';
 import { Injectable } from '@angular/core';
-import { VirtualTimeScheduler } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import {HttpClientModule} from '@angular/common/http';
 import { v4 as uuid } from 'uuid' ;
 
 
@@ -15,18 +11,14 @@ export class NlpAgentService {
  
   constructor(private http: HttpClient) { }
   session_uuid: any;
-  /**
- * createUUID
- */
+
 
   public createUUID() {
    this.session_uuid = uuid();
    console.log("UUID: ", this.session_uuid);
   }
 
-  /**
-  * async sendToBot
-  msg */
+  
   public async sendToBot(msg: string) {
     const url = 'http://localhost:5005/';
     const body = {"sender": this.session_uuid, "message": msg};
