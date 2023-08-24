b0d7631b61c57355563283292b7f944fabced618
nataliasanchezsanchez <47853585+nataliasanchezsanchez@users.noreply.github.com>
2023-08-25 00:42:36 +0530

    Update speech-synthesis.service.spec.ts

--- src/app/services/speech-synthesis.service.spec.ts
+++ src/app/services/speech-synthesis.service.spec.ts
@@ -3,14 +3,15 @@ import { TestBed } from '@angular/core/testing';
 import { SpeechSynthesisService } from './speech-synthesis.service';
 
 describe('SpeechSynthesisService', () => {
-  let service: SpeechSynthesisService;
+  /*let service: SpeechSynthesisService;*/
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SpeechSynthesisService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({}));
+   /* service = TestBed.inject(SpeechSynthesisService);
+  });*/
 
   it('should be created', () => {
+    const service: SpeechSynthesisService=TestBed.get(SpeechSynthesisService);
     expect(service).toBeTruthy();
   });
 });
+
