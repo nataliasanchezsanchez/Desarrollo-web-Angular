95b86c105f26acad7ae4e4832b0cd2b4493e3075
nataliasanchezsanchez <47853585+nataliasanchezsanchez@users.noreply.github.com>
2023-08-25 00:44:29 +0530

    Update voice-recognition.service.spec.ts

--- src/app/services/voice-recognition.service.spec.ts
+++ src/app/services/voice-recognition.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { VoiceRecognitionService } from './voice-recognition.service';
 
 describe('VoiceRecognitionService', () => {
-  let service: VoiceRecognitionService;
+  /*let service: VoiceRecognitionService;*/
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(VoiceRecognitionService);
-  });
+  beforeEach(() =>  TestBed.configureTestingModule({}));
+    /*service = TestBed.inject(VoiceRecognitionService);
+  });*/
 
   it('should be created', () => {
+   const service: VoiceRecognitionService=TestBed.get(VoiceRecognitionService); 
     expect(service).toBeTruthy();
   });
 });
